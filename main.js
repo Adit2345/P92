4daac7d67ec8c14b173a8e31da6114b6a8f57632
Adit2345 <109121747+Adit2345@users.noreply.github.com>
2022-08-17 16:09:51 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var canvas = new fabric.Canvas('myCanvas');
+var x= document.getElementById("myAudio");
+
+function new_image()
+{
+fabric.Image.fromURL('BirthdayImage.jpg', function)
+}
+function playSound(){
+
+}
